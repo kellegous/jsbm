ddb88c514772f9d6394ad5b4c78ca89ac583009f
Kelly Norton <kel@kellegous.com>
2010-10-21 17:02:19 -0400

    Added ThreadNow support for mac.

--- timer.cc
+++ timer.cc
@@ -4,38 +4,42 @@
 #include <mach/mach.h>
 #include <mach/mach_time.h>
 #include <CoreServices/CoreServices.h>
-
-Timer::Timer() : start_time_(mach_absolute_time()) {
-}
-
-double Timer::GetElapsed() {
-  uint64_t elapsed = mach_absolute_time() - start_time_;
-  Nanoseconds nanos = AbsoluteToNanoseconds(*(AbsoluteTime*)&elapsed);
-  return (*(uint64_t*)&nanos) / 1000000.0;
-}
-
 #else
-
 #include <stdint.h>
 #include <time.h>
+#endif
 
 namespace {
 
-uint64_t Now() {
+uint64_t ThreadNow() {
+#if defined(__APPLE__)
+  mach_msg_type_number_t infoCount = THREAD_BASIC_INFO_COUNT;
+  thread_basic_info_data_t info;
+  mach_port_t threadPort = mach_thread_self();
+  if (thread_info(threadPort, THREAD_BASIC_INFO,
+      reinterpret_cast<thread_info_t>(&info), &infoCount) != KERN_SUCCESS) {
+    // TODO(knorton): Handle this better.
+    return 0;
+  }
+  mach_port_deallocate(mach_task_self(), threadPort);
+  return (static_cast<int64_t>(info.user_time.seconds) * Timer::kMicrosecondsPerSecond)
+      + static_cast<int64_t>(info.user_time.microseconds)
+      + (static_cast<int64_t>(info.system_time.seconds) * Timer::kMicrosecondsPerSecond)
+      + static_cast<int64_t>(info.system_time.microseconds);
+#else
   struct timespec ts = {0};
   if (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts) != 0)
     return 0.0; // TODO(knorton): Handle this better.
   return static_cast<int64_t>(ts.tv_sec) * Timer::kMicrosecondsPerSecond +
       (static_cast<int64_t>(ts.tv_nsec) / Timer::kNanosecondsPerMicrosecond);
+#endif
 }
 
 } // namespace
 
-Timer::Timer() : start_time_(Now()) {
+Timer::Timer() : start_time_(ThreadNow()) {
 }
 
 double Timer::GetElapsed() {
-  return (Now() - start_time_) / Timer::kMillisecondsPerSecond;
+  return (ThreadNow() - start_time_) / Timer::kMillisecondsPerSecond;
 }
-
-#endif
